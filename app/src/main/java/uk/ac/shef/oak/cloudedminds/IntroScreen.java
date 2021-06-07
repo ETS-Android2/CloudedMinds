1252e7740bff56cdffb415401e7a5edc2838b155
sukanyac21 <74739878+sukanyac21@users.noreply.github.com>
2021-06-07 21:16:26 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/shef/oak/cloudedminds/IntroScreen.java
@@ -0,0 +1,26 @@
+package uk.ac.shef.oak.cloudedminds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class IntroScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_screen);
+
+        ImageView home = findViewById(R.id.btnHome);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroScreen.this, MainActivity.class));
+            }
+        });
+    }
+}
