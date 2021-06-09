d53cc3b874f583729f2d506ca2a39926a134ec97
sukanyac21 <74739878+sukanyac21@users.noreply.github.com>
2021-06-09 21:24:34 +0100

    Completed Check It Screens

--- app/src/main/java/uk/ac/shef/oak/cloudedminds/CheckIt4.java
+++ app/src/main/java/uk/ac/shef/oak/cloudedminds/CheckIt4.java
@@ -8,27 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class CheckIt3 extends AppCompatActivity {
+public class CheckIt4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_it3);
+        setContentView(R.layout.activity_check_it4);
 
         ImageView home = findViewById(R.id.btnHome);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(CheckIt3.this, MainActivity.class));
+                startActivity(new Intent(CheckIt4.this, MainActivity.class));
             }
         });
 
-        Button checkit4 = findViewById(R.id.btnToCheckIt4);
-        checkit4.setOnClickListener(new View.OnClickListener() {
+        Button checkit5 = findViewById(R.id.btnToCheckIt5);
+        checkit5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(CheckIt3.this, CheckIt4.class));
+                startActivity(new Intent(CheckIt4.this, CheckIt5.class));
             }
         });
+
     }
 }
---
+++ app/src/main/java/uk/ac/shef/oak/cloudedminds/CheckIt5.java
@@ -0,0 +1,14 @@
+package uk.ac.shef.oak.cloudedminds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckIt5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_it5);
+    }
+}
