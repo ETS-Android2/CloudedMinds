64659ffae580f7c1c9158a68ec6365caf022f33a
sukanyac21 <74739878+sukanyac21@users.noreply.github.com>
2021-06-09 17:33:18 +0100

    Check It Layout 3

--- app/src/main/java/uk/ac/shef/oak/cloudedminds/CheckIt3.java
+++ app/src/main/java/uk/ac/shef/oak/cloudedminds/CheckIt3.java
@@ -5,29 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
-public class CheckIt2 extends AppCompatActivity {
+public class CheckIt3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_it2);
+        setContentView(R.layout.activity_check_it3);
 
         ImageView home = findViewById(R.id.btnHome);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(CheckIt2.this, MainActivity.class));
-            }
-        });
-
-        Button checkit3 = findViewById(R.id.btnToCheckIt3);
-        checkit3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(CheckIt2.this, CheckIt3.class));
+                startActivity(new Intent(CheckIt3.this, MainActivity.class));
             }
         });
     }
